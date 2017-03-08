fff86301d6e5f6bf14a4ef1610f33f2c8adfbf22
Emil <pillar_box@bk.ru>
2017-03-08 18:27:21 +0300

    переработал файл авторизации, так как подключение к ldap сделал в файле connect.php с отдельным классом, проверки делаются там же

--- aj/login.php
+++ aj/login.php
@@ -1,22 +1,12 @@
 <?php
 session_start();
-$server = 'blogon3.hq.ru.corp.leroymerlin.com';
-$port = '389';
+require_once '../connect.php';
+
 $user = $_POST["user"];
 $userPW = $_POST["userPW"];
-$dn = 'OU=Shops,OU=Leroy Merlin Vostok,DC=hq,DC=ru,DC=corp,DC=leroymerlin,DC=com';
 
-// Подключаемся к серверу
-$conn = ldap_connect($server, $port) or die('Ошибка: невозможно подключиться к LDAP-серверу');
-// Устанавливаем версию протокола (странно, но по умолчанию не 3-я версия)
-ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+$data = connect_to_ldap($user, $userPW, 'connect.ini');
 
-// Подсоединение
-ldap_bind($conn, "ru1000\\".$user, $userPW) or die('Ошибка: введен некорректный логин, либо пароль');
-
-$result = ldap_search($conn,$dn, "(sAMAccountName=$user)") or die ("Ошибка поиска: ".ldap_error($conn));
-
-//echo $data[0]["title"][0] . "||" . $data[0]["displayname"][0];
 $jobs = array('специалист по информационным технологиям',
 				'менеджер отдела',
 				'контролер управления',
@@ -24,9 +14,8 @@ $jobs = array('специалист по информационным техно
 				'руководитель торгового сектора',
 				'руководитель сектора логистики');
 
-if ($result)
+if ($data)
 {
-	$data = ldap_get_entries($conn, $result);
 	$_SESSION['ldap'] = $user;
 	$_SESSION['username'] = $data[0]["displayname"][0];
 	$_SESSION['job'] = $data[0]["title"][0];
@@ -39,10 +28,6 @@ if ($result)
 	{
 		$_SESSION['root'] = 0;
 	}
-	echo("true");
+	echo 'true';
 }
-
-
-        
-ldap_close($conn);
 ?>
