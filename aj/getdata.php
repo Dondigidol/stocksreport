14aa254a8650ca8166f426a276cdc57d43ad41df
Emil <pillar_box@bk.ru>
2017-03-09 11:24:09 +0300

    изменил структуру запроса, а также данные для вывода

--- aj/getdata.php
+++ aj/getdata.php
@@ -36,95 +36,95 @@ connect_to_mysql('connect.ini');
 $curdate=date("Ymd");
 $lastdate=date("Ymd",strtotime("-1 month"));
 $daysbetween = abs(strtotime($curdate) - strtotime($lastdate)) / (3600 * 24); 
-// echo ("<script>alert(" . $daysbetween . ");</script>");
 
-$sql2=mssql_query("SELECT art.top_repl as top_repl, pr.product_code as lm, name.short_name as name, "
-				."ISNULL((SELECT SUM(bin_qty_stocks) FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE product_id=art.product_id AND bin.description='Reserve deportee'),0) as RD, "
-				."ISNULL((SELECT SUM(bin_qty_stocks) FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE product_id=art.product_id AND bin.description='RM'),0) as RM, "
-				."ISNULL((SELECT SUM(bin_qty_stocks) FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE product_id=art.product_id AND bin.description='EM'),0) as EM, "
-				."ISNULL((SELECT SUM(bin_qty_stocks) FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE product_id=art.product_id AND bin.description='Libre Service'),0) as LS, "
-				."ISNULL((SELECT sum(qty)/" . $daysbetween . " FROM XPERT.dbo.lm_RETAIL_TRAN_FIN_SESS_DT_PROD WHERE (transtype='sale') and (open_date between '" . $lastdate . "' and '" . $curdate . "') and totalamount>0 and product_id=art.product_id), 0) as AVG_SALE "
-				."FROM LMXPERT.dbo.lm_artmag as art "
-				."LEFT JOIN xpert.dbo.product pr ON pr.product_id=art.product_id "
-				."LEFT JOIN XPERT.dbo.product_group prod_g on prod_g.product_group_id=pr.level1_id "
-				."LEFT JOIN xpert.dbo.product_name name ON name.product_id=art.product_id AND name.Language_id=322 "
-				."WHERE prod_g.group_code LIKE '".$otdel."' and art.top_repl LIKE '".$top."' "
-				."ORDER BY prod_g.group_code");
+$query_str = ("SELECT pr.product_code as lm, name.short_name as name, (ISNULL(stock_rd.kol, 0) + ISNULL(stock_rm.kol, 0) + ISNULL(stock_em.kol, 0) + ISNULL(stock_ls.kol, 0)) as kol,ISNULL(stock_rd.kol, 0) as rd, ISNULL(stock_rm.kol, 0) as rm, ISNULL(stock_em.kol, 0) as em, ISNULL(stock_ls.kol, 0) as ls, ISNULL(AVG_SALE.avg_sale, 0) as avg_sale 
+				FROM LMXPERT.dbo.lm_artmag as art
+				LEFT JOIN xpert.dbo.product pr ON pr.product_id=art.product_id
+				LEFT JOIN XPERT.dbo.product_group prod_g on prod_g.product_group_id=pr.level1_id
+				LEFT JOIN xpert.dbo.product_name name ON name.product_id=art.product_id AND name.Language_id=322
+				LEFT JOIN (SELECT stock.product_id as pr_id, SUM(bin_qty_stocks) as kol FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE bin.description='Reserve deportee' Group by stock.product_id) stock_rd ON stock_rd.pr_id = art.product_id
+				LEFT JOIN (SELECT stock.product_id as pr_id, SUM(bin_qty_stocks) as kol FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE bin.description='RM' Group by stock.product_id) stock_rm ON stock_rm.pr_id = art.product_id
+				LEFT JOIN (SELECT stock.product_id as pr_id, SUM(bin_qty_stocks) as kol FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE bin.description='EM' Group by stock.product_id) stock_em ON stock_em.pr_id = art.product_id
+				LEFT JOIN (SELECT stock.product_id as pr_id, SUM(bin_qty_stocks) as kol FROM xpert.dbo.bin_detail AS stock LEFT JOIN xpert.dbo.bin_header bin ON bin.bin_id=stock.bin_id WHERE bin.description='Libre Service' Group by stock.product_id) stock_ls ON stock_ls.pr_id = art.product_id
+				LEFT JOIN (SELECT product_id, sum(qty)/" . $daysbetween . " as avg_sale FROM XPERT.dbo.lm_RETAIL_TRAN_FIN_SESS_DT_PROD WHERE (transtype='sale') and (open_date between '" . $lastdate . "' and '" . $curdate . "') and totalamount>0 Group by product_id) AVG_SALE ON AVG_SALE.product_id = art.product_id
+				WHERE prod_g.group_code = '1' and art.top_repl = '1' and (ISNULL(stock_rd.kol, 0) + ISNULL(stock_rm.kol, 0) + ISNULL(stock_em.kol, 0) + ISNULL(stock_ls.kol, 0) > 0)");
+				
+
+$sql2=mssql_query($query_str);
 
 $i=0;
 while ($result2=mssql_fetch_array($sql2)){
-	$lm=$result2["lm"];
-	$top=$result2["top_repl"];
-	$name=$result2["name"];
-	$rd=$result2["RD"];
-	$rm=$result2["RM"];
-	$em=$result2["EM"];
-	$ls=$result2["LS"];
-	$avg_sale=$result2["AVG_SALE"];
+	$query_str2 = '';
+	$lm = $result2["lm"];
+	$name = $result2["name"];
+	$kol = $result2["kol"];
+	$rd = $result2["rd"];
+	$rm = $result2["rm"];
+	$em = $result2["em"];
+	$ls = $result2["ls"];
+	$avg_sale = $result2["avg_sale"];
 	
-	if (($rd+$rm+$em+$ls)>0){
-		$sql=mysql_query("SELECT IFNULL(SUM(kol), 0), 
-						(SELECT IFNULL(SUM(kol), 0) FROM leroy_stocksreport.capacity WHERE lm= '".$lm."')
-						FROM leroy_stocks.stocks
-						WHERE lm= '".$lm."' AND otdel=".$otdel);
+	$query_str2 = ("SET @ls = (SELECT IFNULL(" . $ls . ", 0));
+					SET @avg_sale = (SELECT IFNULL(" . $avg_sale . ", 0));
+					SET @cap = (SELECT IFNULL(SUM(kol), 0) FROM leroy_stocksreport.capacity WHERE lm= " . $lm . ");
+					SELECT IFNULL(SUM(kol), 0) as top_stocks, (@ls - IFNULL(SUM(kol), 0)) as polka, @cap as capacity, @cap - (@ls - IFNULL(SUM(kol), 0)) as freeplace, ((@ls - IFNULL(SUM(kol), 0)) / @avg_sale) as zapas
+					FROM leroy_stocks.stocks
+					WHERE lm = " . $lm . " AND otdel = " . $otdel);
+			
+	$sql=mysql_query($query_str2);
+	
+	$top_stocks=mysql_fetch_array($sql);
+	echo "<script>alert('" . $top_stocks . "');</script>";
+		$rep[$i]["lm"]=$lm;
+		$rep[$i]["name"]=$name;
+		$rep[$i]["kol"] = $kol;
+		$rep[$i]["rd"] =$rd;
+		$rep[$i]["rm"] =$rm;
+		$rep[$i]["em"] =$em;
+		$rep[$i]["ls"] =$ls;
+		$rep[$i]["AVG_SALE"] =round($avg_sale,1);
+		$rep[$i]["top_stocks"] =$top_stocks["top_stocks"];
+		$rep[$i]["polka"] = $top_stocks["polka"];
+		$rep[$i]["capacity"] =$top_stocks["capacity"];
+		$rep[$i]["freeplace"] = $top_stocks["freeplace"];
+		$rep[$i]["zapas"] = $top_stocks["zapas"];
 		
-		$top_stocks=mysql_fetch_array($sql);
-
-		$rep["lm"][$i]=$lm;
-		$rep["top"][$i]=$top;
-		$rep["name"][$i]=$name;
-		$rep["rd"][$i]=$rd;
-		$rep["rm"][$i]=$rm;
-		$rep["em"][$i]=$em;
-		$rep["ls"][$i]=$ls;
-		$rep["top_stocks"][$i]=$top_stocks[0];
-		$rep["capacity"][$i]=$top_stocks[1];
-		$rep["AVG_SALE"][$i]=round($avg_sale, 1);
 		$i++;
-	}	
-}				
+}
 
 $str='';
-for ($k=0; $k<=$i-1; $k++){
+for ($k=0; $k<=count($rep); $k++){
 	if ($k%2==0){
 		$bgcolor="#dedede";
 	}
 	else{
 		$bgcolor="#ffffff";
 	}
-	$kol=$rep["rd"][$k]+$rep["rm"][$k]+$rep["em"][$k]+$rep["ls"][$k];
-	$polka=$rep["ls"][$k]-$rep["top_stocks"][$k];
-	$freeplace=$rep["capacity"][$k]-$polka;
-	if ($polka!=0 && $rep["AVG_SALE"][$k]!=0){
-		$zapas=round($polka/$rep["AVG_SALE"][$k]);
-	} else {
-		$zapas=0;
-	}
 	
 	if ($root == 1)
 	{
-		$cap = "<td width=".$capacityw."><div><div id='".$k."position".$rep["lm"][$k]."' ondblclick='addvmest(this)' tabindex='".$k."' style='outline: none;'><input type='number' class='cap".$k."' id='".$k."vmest".$rep["lm"][$k]."' style='width:60px;' onblur='savevmest(this);' disabled value=".$rep["capacity"][$k]."></input></div></div></td>";
+		$cap = "<td width=".$capacityw."><div><div id='".$k."position".$rep[$k]["lm"]."' ondblclick='addvmest(this)' tabindex='".$k."' style='outline: none;'><input type='number' class='cap".$k."' id='".$k."vmest".$rep[$k]["lm"]."' style='width:60px;' onblur='savevmest(this);' disabled value=".$rep[$k]["capacity"]."></input></div></div></td>";
 	}
 	else
 	{
-		$cap = "<td width=".$capacityw."><div id='cap".$k."'>".$rep["capacity"][$k]."</div></td>";
+		$cap = "<td width=".$capacityw."><div id='cap".$k."'>".$rep[$k]["capacity"]."</div></td>";
 	}
 	
 	$str=$str."<div id='row".$k."' class='rowel'>
 		<table style='background: ".$bgcolor.";'><tr height='16px'>
-		<td width=".$lmw."><div id='lm".$k."'>".trim($rep["lm"][$k])."</div></td>
-		<td width=".$namew."><div id='name".$k."'>".iconv("cp1251","UTF8",$rep["name"][$k])."</div></td>
-		<td width=".$kolw."><div id='kol".$k."'>".$kol."</div></td>
-		<td width=".$rdw."><div id='rd".$k."'>".$rep["rd"][$k]."</div></td>
-		<td width=".$rmw."><div id='rm".$k."'>".$rep["rm"][$k]."</div></td>
-		<td width=".$emw."><div id='em".$k."'>".$rep["em"][$k]."</div></td>
-		<td width=".$lsw."><div id='ls".$k."'>".$rep["ls"][$k]."</div></td>
-		<td width=".$topstockw."><div id='stock".$k."'>".$rep["top_stocks"][$k]."</div></td>
-		<td width=".$polkaw."><div id='polka".$k."'>".$polka."</div></td>"
+		<td width=".$lmw."><div id='lm".$k."'>".trim($rep[$k]["lm"])."</div></td>
+		<td width=".$namew."><div id='name".$k."'>".iconv("cp1251","UTF8",$rep[$k]["name"])."</div></td>
+		<td width=".$kolw."><div id='kol".$k."'>".$rep[$k]["kol"]."</div></td>
+		<td width=".$rdw."><div id='rd".$k."'>".$rep[$k]["rd"]."</div></td>
+		<td width=".$rmw."><div id='rm".$k."'>".$rep[$k]["rm"]."</div></td>
+		<td width=".$emw."><div id='em".$k."'>".$rep[$k]["em"]."</div></td>
+		<td width=".$lsw."><div id='ls".$k."'>".$rep[$k]["ls"]."</div></td>
+		<td width=".$topstockw."><div id='stock".$k."'>".$rep[$k]["top_stocks"]."</div></td>
+		<td width=".$polkaw."><div id='polka".$k."'>".$rep[$k]["polka"]."</div></td>"
 		. $cap .
-		"<td width=".$freeplacew."><div id='free".$k."'>".$freeplace."</div></td>
-		<td width=".$avg_salew."><div id='sale".$k."'>".number_format($rep["AVG_SALE"][$k], 1, '.', '')."</div></td>
-		<td width=".$zapasw."><div id='zapas".$k."'>".$zapas."</div></td>
+		"<td width=".$freeplacew."><div id='free".$k."'>".$rep[$k]["freeplace"]."</div></td>
+		<td width=".$avg_salew."><div id='sale".$k."'>".$rep[$k]["AVG_SALE"]."</div></td>
+		<td width=".$zapasw."><div id='zapas".$k."'>".$rep[$k]["zapas"]."</div></td>
 		</tr></table></div>";	
 }				
 echo $str;
