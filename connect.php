d7b84d1bc7dbbea8143f96fb3aa7fc863fa34e17
Emil <pillar_box@bk.ru>
2017-03-09 08:30:14 +0300

    добавлен класс подключения к MySQL серверу

--- connect.php
+++ connect.php
@@ -66,6 +66,28 @@ class mssql_connection
 	}
 }
 
+class mysql_connection
+{
+	var $server;
+	var $base;
+	var $user;
+	var $userPW;
+	function get_params($ini_file)
+	{
+		$params_arr = parse_ini_file($ini_file);
+		isset($params_arr['mysql_server']) ? $this->server = $params_arr['mysql_server'] : die('В файле конфигурации нет данных о MySQL сервере');
+		isset($params_arr['mysql_base']) ? $this->base = $params_arr['mysql_base'] : die('В файле конфигурации нет данных о подключаемой базе MySQL сервера');
+		isset($params_arr['mysql_user']) ? $this->user = $params_arr['mysql_user'] : die('В файле конфигурации нет данных о логине подключения к MySQL серверу');
+		isset($params_arr['mysql_password']) ? $this->userPW = $params_arr['mysql_password'] : die('В файле конфигурации нет данных о пароле подключения к MySQL серверу');
+	}
+	
+	function set_connection()
+	{
+		$link = mysql_connect($this->server, $this->user, $this->userPW) or die("Невозможно подключиться к серверу MySQL!");
+		mysql_select_db($this->base, $link) or die("Невозможно подключиться к Базе Данных MySQL!"); 
+	}
+}
+
 function connect_to_ldap($user, $userPW, $ini_file)
 {
 	$ldap = new ldap_connection;
@@ -80,6 +102,13 @@ function connect_to_mssql($ini_file)
 	$mssql = new mssql_connection;
 	$mssql->get_params($ini_file);
 	$mssql->set_connection();
+}
+
+function connect_to_mysql($ini_file)
+{
+	$mysql = new mysql_connection;
+	$mysql->get_params($ini_file);
+	$mysql->set_connection();
 }
  
 ?>
