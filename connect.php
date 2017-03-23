2072123b99e64d4cbadee2beff4de667a96a1e72
Dondigidol <pillar_box@bk.ru>
2017-03-24 00:34:07 +0300

    добавил локальный репозиторий ноутбука

--- connect.php
+++ connect.php
@@ -47,46 +47,83 @@ class ldap_connection
 class mssql_connection
 {
 	var $server;
-	var $base;
+	var $database;
 	var $user;
 	var $userPW;
+	var $conn;
 	function get_params($ini_file)
 	{
 		$params_arr = parse_ini_file($ini_file);
 		isset($params_arr['mssql_server']) ? $this->server = $params_arr['mssql_server'] : die('В файле конфигурации нет данных о MSSQL сервере');
-		isset($params_arr['mssql_base']) ? $this->base = $params_arr['mssql_base'] : die('В файле конфигурации нет данных о подключаемой базе MSSQL сервера');
+		isset($params_arr['mssql_database']) ? $this->database = $params_arr['mssql_database'] : die('В файле конфигурации нет данных о подключаемой базе MSSQL сервера');
 		isset($params_arr['mssql_user']) ? $this->user = $params_arr['mssql_user'] : die('В файле конфигурации нет данных о логине подключения к MSSQL серверу');
 		isset($params_arr['mssql_password']) ? $this->userPW = $params_arr['mssql_password'] : die('В файле конфигурации нет данных о пароле подключения к MSSQL серверу');
 	}
 	
 	function set_connection()
 	{
-		$link = mssql_connect($this->server, $this->user, $this->userPW) or die("Невозможно подключиться к серверу MSSQL!");
-		mssql_select_db($this->base, $link) or die("Невозможно подключиться к Базе Данных MSSQL!"); 
+		try
+		{
+			$this->conn = odbc_connect('Driver={SQL Server};Server=' . $this->server . ';Database=' . $this->database . ';', $this->user, $this->userPW);
+		
+		}
+		catch (Exception $e)
+		{
+			echo 'Ошибка при подключении к MSSQL серверу:' . $e->getMessage() . '\n';
+		}
+		
+	}
+	
+	function sql_query($query_str)
+	{
+		if (isset($this->conn))
+		{
+			$result = array();
+			try 
+			{					
+				ini_set('max_execution_time', 720);
+				$arr = odbc_exec($this->conn, $query_str);
+				while($val = odbc_fetch_array($arr))
+				{
+					array_push($result, $val);
+				}
+				// $result = iconv("UTF-8", "CP1252", $result);
+				// odbc_free_result($result);
+				// return $data;				
+			}
+			catch (Exception $e)
+			{
+				echo 'Ошибка при выполнении запроса' . $e->getmessage() . '\n';
+			}
+			finally
+			{
+				return $result;//$data[];
+			}
+		}
 	}
 }
 
 class mysql_connection
 {
 	var $server;
-	var $base;
+	var $database;
 	var $user;
 	var $userPW;
 	function get_params($ini_file)
 	{
 		$params_arr = parse_ini_file($ini_file);
 		isset($params_arr['mysql_server']) ? $this->server = $params_arr['mysql_server'] : die('В файле конфигурации нет данных о MySQL сервере');
-		isset($params_arr['mysql_base']) ? $this->base = $params_arr['mysql_base'] : die('В файле конфигурации нет данных о подключаемой базе MySQL сервера');
+		isset($params_arr['mysql_database']) ? $this->database = $params_arr['mysql_database'] : die('В файле конфигурации нет данных о подключаемой базе MySQL сервера');
 		isset($params_arr['mysql_user']) ? $this->user = $params_arr['mysql_user'] : die('В файле конфигурации нет данных о логине подключения к MySQL серверу');
 		isset($params_arr['mysql_password']) ? $this->userPW = $params_arr['mysql_password'] : die('В файле конфигурации нет данных о пароле подключения к MySQL серверу');
 	}
 	
 	function set_connection()
 	{
-		$link = mysql_connect($this->server, $this->user, $this->userPW) or die("Невозможно подключиться к серверу MySQL!");
-		mysql_select_db($this->base, $link) or die("Невозможно подключиться к Базе Данных MySQL!");
-		mysql_query("SET NAMES 'utf8'");
+		$link = mysqli_connect($this->server, $this->user, $this->userPW, $this->database) or die("Невозможно подключиться к серверу MySQL!");
+		mysqli_query($link, "SET NAMES 'utf8'");
 		ini_set('max_execution_time', 720);
+		return $link;
 	}
 }
 
@@ -104,6 +141,7 @@ function connect_to_mssql($ini_file)
 	$mssql = new mssql_connection;
 	$mssql->get_params($ini_file);
 	$mssql->set_connection();
+	return $mssql;
 }
 
 function connect_to_mysql($ini_file)
@@ -111,6 +149,7 @@ function connect_to_mysql($ini_file)
 	$mysql = new mysql_connection;
 	$mysql->get_params($ini_file);
 	$mysql->set_connection();
+	return $mysql;
 }
  
 ?>
